3163e1b777a7fa5460762f7be257c0ee8ec44009
Izhan Akhtar <izhanakhtar255@gmail.com>
2024-09-04 15:33:55 +0400

    Python1

---
+++ Fuctiontest.py
@@ -0,0 +1,12 @@
+def bday_Message():
+    Name= input("Enter your Name")
+    Birth_Month= input("Enter your Birth Month")
+    Birth_Day_Of_The_Month= input("Enter your Birth Day of the Month")
+    Birth_Year= input("Enter your Birth Year")
+
+    print(Name, "your birthday is on" , Birth_Month, Birth_Day_Of_The_Month,",", Birth_Year)
+
+bday_Message()
+bday_Message()
+bday_Message()
+
